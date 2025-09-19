0bf28eeb43b598d80e3c4559d47e38a18c772b30
yenshow <yenshow2020@gmail.com>
2025-09-19 09:18:47 +0800

    page change animate

--- app/router.options.ts
+++ app/router.options.ts
@@ -6,12 +6,6 @@ export default <RouterConfig>{
 		if (savedPosition) {
 			return savedPosition;
 		}
-
-		// 語言切換時滾動到頂端
-		if (from && to && from.path !== to.path && from.params?.locale !== to.params?.locale) {
-			return { top: 0, behavior: "smooth" };
-		}
-
 		// 如果目標路由帶有 hash (錨點)
 		if (to.hash) {
 			return {
@@ -19,8 +13,6 @@ export default <RouterConfig>{
 				behavior: "smooth"
 			};
 		}
-
-		// 預設滾動到頂端
-		return { top: 0, behavior: "smooth" };
+		return { top: 0 };
 	}
 };
